35f7c208f801e3c45faec9ecf062c92f02fe42c9
SwapnilPatil <swapnil.patil04@gmail.com>
2021-02-07 02:29:23 +0530

    work on session

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  devise_for :student_exams
-                                  
+  devise_for :student_exams, controllers: {
+               sessions: 'student_exams/sessions'
+             }
+  
+  
   resources :exams, only: [] do
     resources :register, only: [:new, :create]
   end
